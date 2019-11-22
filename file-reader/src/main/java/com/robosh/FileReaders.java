35f0ad819184e3d49f8e0eb100b137bf242e9581
Orest Shemeliuk <orestshemelyuk@gmail.com>
2019-11-22 05:11:29 +0200

    create files task: laba4

---
+++ file-reader/src/main/java/com/robosh/FileReaders.java
@@ -0,0 +1,44 @@
+package com.robosh;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+/**
+ * {@link FileReaders} privides an API that allow to read whole file into a {@link String} by file name.
+ */
+public class FileReaders {
+
+    /**
+     * Returns a {@link String} that contains whole text from the file specified by name.
+     *
+     * @param fileName a name of a text file
+     * @return string that holds whole file content
+     */
+    public static String readWholeFile(String fileName) {
+        Path filePath = getPathFromFileName(fileName);
+        try {
+            return Files.lines(filePath)
+                    .collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    private static Path getPathFromFileName(String fileName) {
+        URL fileUrl = FileReaders.class.getClassLoader().getResource(fileName);
+        if (fileUrl == null) {
+            throw new RuntimeException("No such URL");
+        }
+
+        try {
+            return Paths.get(fileUrl.toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("No such URI");
+        }
+    }
+}
---
+++ file-stats/src/main/java/com/robosh/FileStats.java
@@ -0,0 +1,97 @@
+package com.robosh;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.*;
+
+/**
+ * {@link FileStats} provides an API that allow to get character statistic based on text file. All whitespace characters
+ * are ignored.
+ */
+public class FileStats {
+
+    private Map<Character, Long> charactersMap;
+
+    /**
+     * Creates a new immutable {@link FileStats} objects using data from text file received as a parameter.
+     *
+     * @param fileName input text file name
+     * @return new FileStats object created from text file
+     */
+    public static FileStats from(String fileName) {
+        return new FileStats(fileName);
+    }
+
+    private FileStats(String fileName) {
+        Path filePath = getPathFromFileName(fileName);
+        try {
+            List<String> strings = Files.readAllLines(filePath);
+            charactersMap = collectCharactersToCountMap(strings.stream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static Path getPathFromFileName(String fileName) {
+        URL fileUrl = FileStats.class.getClassLoader().getResource(fileName);
+        if (fileUrl == null) {
+            throw new FileStatsException("No such URL");
+        }
+
+        try {
+            return Paths.get(fileUrl.toURI());
+        } catch (URISyntaxException e) {
+            throw new FileStatsException("No such file");
+        }
+    }
+
+    private Map<Character, Long> collectCharactersToCountMap(Stream<String> linesStream) {
+        return linesStream
+                .flatMap(str -> str.chars().mapToObj(ch -> (char) ch).collect(toList()).stream())
+                .filter(ch -> ch != ' ') // filter whitespace
+                .collect(groupingBy(identity(), counting()));
+    }
+
+    public int getCharCount(char character) {
+        return Math.toIntExact(charactersMap.get(character));
+    }
+
+    /**
+     * Returns a character that appeared most often in the text.
+     *
+     * @return the most frequently appeared character
+     */
+    public char getMostPopularCharacter() {
+        return charactersMap.entrySet()
+                .stream()
+                .max(Comparator.comparing(Map.Entry::getValue))
+                .orElseThrow(() -> new FileStatsException("There is no characters"))
+                .getKey();
+    }
+
+    public long getSumCharacters() {
+        return charactersMap.values().stream().reduce(0L, (aLong, aLong2) -> aLong + aLong2);
+    }
+
+    /**
+     * Returns {@code true} if this character has appeared in the text, and {@code false} otherwise
+     *
+     * @param character a specific character to check
+     * @return {@code true} if this character has appeared in the text, and {@code false} otherwise
+     */
+    public boolean containsCharacter(char character) {
+        return charactersMap.containsKey(character);
+    }
+}
