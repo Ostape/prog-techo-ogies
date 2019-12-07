7c1e8b68214588477fa1e39258d02abd1a20be35
Orest Shemeliuk <orestshemelyuk@gmail.com>
2019-12-07 20:36:21 +0200

    create advaned and middle course classes

---
+++ pattern/src/main/java/com/robosh/model/entity/course/AdvancedCourse.java
@@ -0,0 +1,8 @@
+package com.robosh.model.entity.course;
+
+public class AdvancedCourse implements Course {
+    @Override
+    public String learnJava() {
+        return "Repeat Java basics. Learn Spring Framework: Spring MVC, Spring Security. Learn REST.";
+    }
+}
---
+++ pattern/src/main/java/com/robosh/model/entity/course/MiddleCourse.java
@@ -0,0 +1,8 @@
+package com.robosh.model.entity.course;
+
+public class MiddleCourse implements Course {
+    @Override
+    public String learnJava() {
+        return "Learn Java basic. Learn Java Collection framework advanced. Learn java concurrency package.";
+    }
+}
